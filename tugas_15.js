f747af94482c772c8675ffd2306109e5bce9a62c
AndriantoAGun <andrianto22491@gmail.com>
2019-07-22 21:19:49 +0700

    Tugas 15 JavaScript
    tugas Niomic tentang JavaScript meliputi Math.Pow dan Math.Sqrt

---
+++ tugas_15.js
@@ -0,0 +1,9 @@
+var ab = 8;
+var bc = 6;
+var abpow = Math.pow(8,2);
+var bcpow = Math.pow(6,2);
+var ac = Math.sqrt(abpow + bcpow);
+
+console.log("Nilai AB : "+ ab);
+console.log("Nilai BC : "+ bc);
+console.log("panjang sisi AC pada segitiga siku-siku tersebut adalah : "+ ac +" cm");
